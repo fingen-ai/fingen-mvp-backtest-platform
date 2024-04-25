9f91b950be8afc34df9ea2e76106e4ad76f53993
fingen <fingen.ai@gmail.com>
2024-04-24 21:54:26 -0600

    Next Steps: 1. Integrate BassoTrendStrategy - WIP.

--- src/main/java/core/service/strategy/StrategyPubImpl.java
+++ src/main/java/core/service/strategy/StrategyPubImpl.java
@@ -1,10 +1,15 @@
 package core.service.strategy;
 
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.queue.ExcerptAppender;
+import net.openhft.chronicle.queue.ExcerptTailer;
+import net.openhft.chronicle.queue.RollCycles;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 import strategies.trend.basso.BassoTrendStrategy;
 import strategies.trend.basso.BassoTrendStrategyImpl;
 
 import java.io.IOException;
-import java.sql.SQLSyntaxErrorException;
 
 public class StrategyPubImpl implements StrategyPub, StrategyHandler<StrategyPub> {
 
@@ -15,7 +20,13 @@ public class StrategyPubImpl implements StrategyPub, StrategyHandler<StrategyPub
     private double[] low = new double[50];
     private double[] close = new double[50];
     private int i = 0;
-    String bassoTrendStrategyDecision = null;
+
+    String bassoOrderIdea = null;
+    String basePath = OS.TMP + "/HiveMain/Queues/orderIdeaQ";
+    private ExcerptAppender appender;
+    SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(basePath)
+            .rollCycle(RollCycles.DAILY) // Set the roll cycle for new queue files
+            .build();
 
     BassoTrendStrategy bassoTrendStrategy = new BassoTrendStrategyImpl();
 
@@ -34,16 +45,64 @@ public class StrategyPubImpl implements StrategyPub, StrategyHandler<StrategyPub
         close[i] = strategyData.close;
         prices[i] = (low[i] + high[i] + close[i]) / 3;
 
-        i++;
+        if(i == 49) {
 
-        if(prices.length >= 50) {
-            bassoTrendStrategyDecision = bassoTrendStrategy.getStrategyDecision(prices, high, low, close);
-            System.out.println("Decision: " + bassoTrendStrategyDecision);
+            bassoOrderIdea = bassoTrendStrategy.getStrategyDecision(prices, high, low, close);
+            System.out.println("Print Basso Order Idea: " + bassoOrderIdea);
+
+            this.appender = queue.acquireAppender();
+            appender.writeText(bassoOrderIdea);
+
+            removeTheElement(low, 0);
+            removeTheElement(high, 0);
+            removeTheElement(close, 0);
+            removeTheElement(prices, 0);
+
+            i = 49;
+
+            ExcerptTailer tailer = queue.createTailer();
+            String text;
+            while ((text = tailer.readText()) != null) {
+                System.out.println("Read Basso Order Idea: " + text);
+            }
+
+        } else {
+            i++;
         }
 
         strategyData.svcStopTs = System.nanoTime();
         strategyData.svcLatency = strategyData.svcStopTs - strategyData.svcStartTs;
         System.out.println("STRATEGY: " + strategyData);
         output.simpleCall(strategyData);
+    }
+
+    // Function to remove the element
+    private static double[] removeTheElement(double[] arr, int index)
+    {
+        if (arr == null || index < 0
+                || index >= arr.length) {
+            return arr;
+        }
+
+        // Create another array of size one less
+        double[] updateArray = new double[arr.length - 1];
+
+        // Copy the elements except the index
+        // from original array to the other array
+        for (int i = 0, k = 0; i < arr.length; i++) {
+
+            // if the index is
+            // the removal element index
+            if (i == index) {
+                continue;
+            }
+
+            // if the index is not
+            // the removal element index
+            updateArray[k++] = arr[i];
+        }
+
+        // return the resultant array
+        return updateArray;
     }
 }
