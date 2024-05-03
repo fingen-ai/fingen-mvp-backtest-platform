b0bfaab1788168f62124d3dcabfd43332956d092
fingen <fingen.ai@gmail.com>
2024-05-03 10:38:57 -0600

    OEMS Test - WIP.

--- src/main/java/core/service/oems/OEMSPubImpl.java
+++ src/main/java/core/service/oems/OEMSPubImpl.java
@@ -17,7 +17,33 @@ public class OEMSPubImpl implements OEMSPub, OEMSHandler<OEMSPub> {
     public void simpleCall(OEMSData oemsData) {
         oemsData.svcStartTs = System.nanoTime();
 
-        // get open orders
+        // OMS-IN OPEN ORDER
+        // place new order (single, market)
+
+        // place new order (single, limit)
+
+        // place new order (single, stop)
+
+        // place new order (single, trailing stop)
+
+        // OMS-IN CLOSE ORDER
+        // place new order (single, market)
+
+        // place new order (single, limit)
+
+        // place new order (single, stop)
+
+        // place new order (single, trailing stop)
+
+        // OMS-OUT REPORTING
+        // get all orders
+
+        // get all trades
+
+        // positions
+        // get reports of all states: order, trade, position
+
+        // get reports of all activity: order, trade, position
 
         oemsData.svcStopTs = System.nanoTime();
         oemsData.svcLatency = oemsData.svcStopTs - oemsData.svcStartTs;
