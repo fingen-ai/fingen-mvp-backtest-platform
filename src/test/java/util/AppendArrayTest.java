c445c05b927d90701dca06d444d840792b2d2181
fingen <fingen.ai@gmail.com>
2024-05-05 21:41:03 -0600

    OEMS & OEMSTest - WIP. ExecutionReport - WIP. OMSIn - WIP. AppendArray - DONE.

---
+++ src/test/java/util/AppendArrayTest.java
@@ -0,0 +1,19 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class AppendArrayTest {
+
+    @Test
+    public void testAppendArray() {
+        AppendArray appendArray = new AppendArray();
+
+        double[] array = {1.0, 2.0, 3.0};
+        double x = 4.0;
+        int length = 4;
+
+        double[] expected = {1.0, 2.0, 3.0, 4.0}; // Adjusted expected array length
+        assertArrayEquals(expected, appendArray.appendArray(array, x, length));
+    }
+}
