bbdb8d020b60a773097bdb333200d9c3883eff1b
fingen <fingen.ai@gmail.com>
2024-05-06 23:20:36 -0600

    OEMS & OEMSTest - WIP. ExecutionReport - WIP. OMSIn - WIP.

--- src/main/java/core/service/oems/OEMSPubImpl.java
+++ src/main/java/core/service/oems/OEMSPubImpl.java
@@ -1,15 +1,11 @@
 package core.service.oems;
 
-import oems.dto.CloseOrderAll;
-import oems.dto.NewOrderSingle;
 import oems.map.MapManager;
 
 import java.io.IOException;
 
 public class OEMSPubImpl implements OEMSPub, OEMSHandler<OEMSPub> {
 
-    NewOrderSingle nos = new NewOrderSingle();
-    CloseOrderAll coa = new CloseOrderAll();
     MapManager mm = new MapManager(CharSequence.class, OEMSData.class, 1000);
 
     private OEMSPub output;
@@ -24,26 +20,12 @@ public class OEMSPubImpl implements OEMSPub, OEMSHandler<OEMSPub> {
     public void simpleCall(OEMSData oemsData) throws IOException {
         oemsData.svcStartTs = System.nanoTime();
 
-        addNOS(oemsData);
-        updateNOS(oemsData);
-        closeNOS(oemsData);
+        oemsData.openOrderId = String.valueOf(System.nanoTime());
+        mm.add(oemsData.openOrderId, oemsData);
 
         oemsData.svcStopTs = System.nanoTime();
         oemsData.svcLatency = oemsData.svcStopTs - oemsData.svcStartTs;
         System.out.println("OEMS: " + oemsData);
         output.simpleCall(oemsData);
-    }
-
-    private void addNOS(OEMSData oemsData) {
-        mm.add(oemsData.symbol, oemsData);
-        System.out.println("OEMS ADD: " + oemsData);
-    }
-
-    private void updateNOS(OEMSData oemsData) {
-        System.out.println("OEMS UPDT: " + oemsData);
-    }
-
-    private void closeNOS(OEMSData oemsData) {
-        System.out.println("OEMS CLOSE: " + oemsData);
     }
 }
--- src/main/java/oems/dto/NewOrderSingle.java
+++
@@ -1,7 +0,0 @@
-package oems.dto;
-
-import net.openhft.chronicle.wire.SelfDescribingMarshallable;
-
-public class NewOrderSingle extends SelfDescribingMarshallable {
-
-}
