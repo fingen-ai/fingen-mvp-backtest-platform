8e05f831f75e9ffe721c536c22af57b23e875a2a
fingen <fingen.ai@gmail.com>
2024-05-07 19:31:06 -0600

    OEMS & OEMSTest - WIP. ExecutionReport - WIP. OMSIn - WIP.

--- src/main/java/core/service/insight/InsightPubImpl.java
+++ src/main/java/core/service/insight/InsightPubImpl.java
@@ -1,7 +1,6 @@
 package core.service.insight;
 
 import account.AccountData;
-import oems.map.DoubleArrayMapManager;
 import performance.Performance;
 import performance.PerformanceImpl;
 import risk.Risk;
@@ -30,125 +29,11 @@ public class InsightPubImpl implements InsightPub, InsightHandler<InsightPub> {
     }
 
     public void simpleCall(InsightData insightData) throws IOException {
-
-        DoubleArrayMapManager damm = new DoubleArrayMapManager(1000, "BTC_USD", 1);
         insightData.svcStartTs = System.nanoTime();
-
-        if(insightData.bassoOrderIdea.equals("")) {
-            // Have existing pos - update SL/TP
-            nosArray = damm.get("BTC_USD");
-            updateInsight(insightData);
-
-        } else {
-
-            if(insightData.bassoOrderIdea.equals("Bullish")) {
-                // Have existing pos, But diff direction?
-                nosArray = damm.get("BTC_USD");
-                coaInsight(insightData);
-                nosInsight(insightData);
-
-                // Have existing pos, But same direction?
-                updateInsight(insightData);
-                // Add more to the position?
-                nosInsight(insightData);
-
-                // Have no pos?
-                nosInsight(insightData);
-            }
-
-            if(insightData.bassoOrderIdea.equals("Bearish")) {
-                // Have existing pos, But diff direction?
-                coaInsight(insightData);
-                nosInsight(insightData);
-
-                // Have existing pos, But same direction?
-                updateInsight(insightData);
-                // Add more to the position?
-                nosInsight(insightData);
-
-                // Have no pos?
-                nosInsight(insightData);
-            }
-        }
-
-        /*
-        insightData.atr = atr50.update(insightData.high, insightData.low, insightData.close, insightData.priorClose);
-        insightData.tradeCount = performance.getTradeCount();
-
-        // Trade instructions
-        if(insightData.tradeCount == 0) {
-
-            // Initial instructions
-            insightData.nav = performance.getInitialInvestment();
-            insightData.riskInitPercentThreshold = risk.getInitRiskPercentThreshold();
-            insightData.volInitPercentThreshold = risk.getInitVolPercentThreshold();
-
-            if(insightData.bassoOrderIdea != null) {
-                insightData.tradeDecisionInstruction = "Trade";
-                insightData.tradeQtyPerRiskInstruction = risk.updateOngoingRiskAmtThreshold(insightData.nav, insightData.riskInitPercentThreshold);
-                insightData.tradeQtyPerVolInstruction = risk.updateOngoingVolAmtThreshold(insightData.nav, insightData.volInitPercentThreshold);
-
-            } else {
-                insightData.tradeDecisionInstruction = "Do Not Trade";
-            }
-
-        } else {
-
-            // Ongoing instructions
-            insightData.nav = accountData.nav;
-            insightData.positionRisk = risk.getCurrentTotalPercentRisk(accountData.positionAmt, accountData.nav);
-
-            insightData.riskOngoingPercentThreshold = risk.getOngoingRiskPercentThreshold();
-            insightData.volOngoingPercentThreshold = risk.getOngoingVolPercentThreshold();
-
-            if(insightData.bassoOrderIdea != null) {
-
-                if(insightData.currentTotalPercentRiskPercent < insightData.riskOngoingPercentThreshold) {
-                    insightData.tradeDecisionInstruction = "Trade";
-                    insightData.tradeQtyPerRiskInstruction = risk.updateOngoingRiskAmtThreshold(insightData.nav, insightData.riskOngoingPercentThreshold);
-
-                } else {
-                    insightData.tradeDecisionInstruction = "Do Not Trade";
-                    insightData.tradeQtyPerRiskInstruction = 0;
-                }
-
-                if(insightData.currentTotalPercentVolRiskPercent < insightData.volOngoingPercentThreshold) {
-                    insightData.tradeDecisionInstruction = "Trade";
-                    insightData.tradeQtyPerVolInstruction = risk.updateOngoingVolAmtThreshold(insightData.nav, insightData.volOngoingPercentThreshold);
-                } else {
-                    insightData.tradeDecisionInstruction = "Do Not Trade";
-                    insightData.tradeQtyPerVolInstruction = 0;
-                }
-
-            } else {
-
-                insightData.tradeDecisionInstruction = "Do Not Trade";
-            }
-
-            // Decide trade amt instruction
-            insightData.tradeQtyInstruction = Math.min(insightData.tradeQtyPerRiskInstruction, insightData.tradeQtyPerVolInstruction);
-        }
-
-        // Calc current total risk
-        insightData.currentTotalPercentRiskPercent = risk.getCurrentTotalPercentRisk(insightData.positionRisk, insightData.nav);
-        insightData.currentTotalPercentVolRiskPercent = risk.getCurrentTotalVolPercentRisk(insightData.atr, insightData.nav);
-        */
 
         insightData.svcStopTs = System.nanoTime();
         insightData.svcLatency = insightData.svcStopTs - insightData.svcStartTs;
         System.out.println("INSIGHT: " + insightData);
         output.simpleCall(insightData);
-    }
-
-    private void nosInsight(InsightData insightData) {
-        insightData.nosOrderInsight = "NOS";
-    }
-
-    private void updateInsight(InsightData insightData) {
-        insightData.updOrderInsight = "UPD";
-    }
-
-    private void coaInsight(InsightData insightData) {
-        insightData.coaOrderInsight = "COA";
     }
 }
--- src/main/java/core/service/oems/OEMSPubImpl.java
+++ src/main/java/core/service/oems/OEMSPubImpl.java
@@ -1,14 +1,8 @@
 package core.service.oems;
 
-import oems.map.MapManager;
-
 import java.io.IOException;
 
 public class OEMSPubImpl implements OEMSPub, OEMSHandler<OEMSPub> {
-
-    MapManager mm = new MapManager(CharSequence.class, OEMSData.class, 1000);
-    double[] nosArray = null;
-    double[] coaArray = null;
 
     private OEMSPub output;
 
@@ -21,22 +15,6 @@ public class OEMSPubImpl implements OEMSPub, OEMSHandler<OEMSPub> {
 
     public void simpleCall(OEMSData oemsData) throws IOException {
         oemsData.svcStartTs = System.nanoTime();
-
-        oemsData.openOrderId = String.valueOf(System.nanoTime());
-
-        if(oemsData.nosOrderInsight.equals("NOS")) {
-            mm.add(oemsData.openOrderId, oemsData);
-        }
-
-        if(oemsData.updOrderInsight.equals("UPD")) {
-            mm.update(oemsData.openOrderId, oemsData);
-            mm.add(oemsData.openOrderId, oemsData);
-        }
-
-        if(oemsData.coaOrderInsight.equals("COA")) {
-            mm.delete(oemsData.openOrderId);
-            mm.add(oemsData.openOrderId, oemsData);
-        }
 
         oemsData.svcStopTs = System.nanoTime();
         oemsData.svcLatency = oemsData.svcStopTs - oemsData.svcStartTs;
