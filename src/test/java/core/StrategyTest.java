ee12e469966fe78f0aacb047f7c363eae5ddef02
fingen <fingen.ai@gmail.com>
2024-05-13 18:23:11 -0600

    STRATEGY - DONE. Strategy needs scenarios tests to ensure signal logic working, given the test DXY file.
    INSIGHT - WIP.
    Insight needs scenarios tests to ensure signal logic working, given the test DXY file.
    
    OEMS - WIP.
    OEMS needs scenarios tests to ensure signal logic working, given the test DXY file.

--- src/test/java/core/StrategyTest.java
+++ src/test/java/core/StrategyTest.java
@@ -166,7 +166,40 @@ public class StrategyTest {
             assertEquals("Mismatch in some Strategy field", "Bearish", actual.bassoOrderIdea);
         }
 
+        if((recCount > 1141) && (recCount < 1153)) {
+            System.out.println(recCount + " recs");
+            assertEquals("Mismatch in some Strategy field", "Bullish", actual.bassoOrderIdea);
+        }
 
+        if(recCount == 1153) {
+            System.out.println(recCount + " recs");
+            assertEquals("Mismatch in some Strategy field", "Neutral", actual.bassoOrderIdea);
+        }
+
+        if((recCount > 1153) && (recCount < 1162)) {
+            System.out.println(recCount + " recs");
+            assertEquals("Mismatch in some Strategy field", "Bullish", actual.bassoOrderIdea);
+        }
+
+        if(recCount == 1162) {
+            System.out.println(recCount + " recs");
+            assertEquals("Mismatch in some Strategy field", "Neutral", actual.bassoOrderIdea);
+        }
+
+        if((recCount > 1162) && (recCount < 1173)) {
+            System.out.println(recCount + " recs");
+            assertEquals("Mismatch in some Strategy field", "Bullish", actual.bassoOrderIdea);
+        }
+
+        if(recCount == 1173) {
+            System.out.println(recCount + " recs");
+            assertEquals("Mismatch in some Strategy field", "Neutral", actual.bassoOrderIdea);
+        }
+
+        if((recCount > 1173) && (recCount < 1185)) {
+            System.out.println(recCount + " recs");
+            assertEquals("Mismatch in some Strategy field", "Bullish", actual.bassoOrderIdea);
+        }
 
         recCount++;
     }
